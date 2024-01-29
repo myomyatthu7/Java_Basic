766eabecdae6b0f0ddd09642c7e1796a5b3e7489
MyoMyatThu <myomyatthu19999@gmail.com>
2024-01-29 16:58:55 +0630

    inheritance_update

---
+++ src/inheritance/this_super/MultipleInheritance.java
@@ -0,0 +1,31 @@
+package inheritance.this_super;
+
+// can't Multiple Inheritance in java.but c++ can;
+// can Multi level Inheritance in java.
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Y y = new Y();
+        y.showName();
+    }
+}
+class X {
+    public void showName() {
+        System.out.println("X name");
+    }
+}
+class Y extends X {
+    public void showAge() {
+        System.out.println("Y age");
+    }
+
+    @Override
+    public void showName() {
+        //super.showName();
+        System.out.println("Y name");
+    }
+}
+class Z extends Y {
+    public void showId() {
+        System.out.println("Z id");
+    }
+}
